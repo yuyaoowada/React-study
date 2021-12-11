19c3ba46e7db50d4f89e82f33fb750cab67356f7
yuyaoowada <56225385+yuyaoowada@users.noreply.github.com>
2021-12-11 21:45:28 +0900

    css style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { useState } from "react";
+import { InlineStyle } from "./components/InlineStyle";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>{count} </p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <InlineStyle />
     </div>
   );
 }
